4ff11cbdebd023ac81bc5903892e8ed8c6b6eb7d
BRUNO DIAS GONÇALVES <24122451@CFP127ALUNO.LOCAL>
2025-09-18 16:16:34 -0300

    criaçao das pastas nescessarias

--- app/src/main/java/com/example/oportunyfam/MainActivity.kt
+++ app/src/main/java/com/example/oportunyfam/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.oportunyfam.ui.theme.OportunyFamTheme
+import com.example.tcc.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +23,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             OportunyFamTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "HomeScreen"
+                    ) {
+                        composable("HomeScreen") {
+                            HomeScreen(navController)
+                        }
+                    }
                 }
             }
         }
     }
 }
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
---
+++ app/src/main/java/com/example/oportunyfam/Service/RetrofitFactory.kt
@@ -0,0 +1,4 @@
+package com.example.oportunyfam.Service
+
+class RetrofitFactory {
+}
